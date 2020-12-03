c9f1a12bcf604f3c08b9fe556dffdc6307b8879b
James Stevenson <james.stevenson@nationwidechildrens.org>
2020-12-03 16:26:46 -0500

    Update to latest therapy-normalization codebase

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 """Main application for FastAPI"""
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 from fastapi.openapi.utils import get_openapi
 from typing import Optional
-from gene.query import normalize, InvalidParameterException
+from gene.query import Normalizer, InvalidParameterException
 import html
 
 
-app = FastAPI()
+normalizer = Normalizer()
+app = FastAPI(docs_url='/')
 
 
 def custom_openapi():
@@ -16,36 +17,65 @@ def custom_openapi():
     openapi_schema = get_openapi(
         title="Gene Normalizer",
         version="0.1.0",
-        description="TODO Description here",  # TODO
+        openapi_version="3.0.3",
+        description="Normalize gene terms.",  # TODO
         routes=app.routes
     )
-    # openapi_schema['info']['license'] = {  # TODO
-    #     "name": "name of our license",
-    #     "url": "link to it"
-    # }
-    # openapi_schema['info']['contact'] = {  # TODO
-    #     "name": "alex wagner",
-    #     "email": "his email"
-    # }
+    openapi_schema['info']['license'] = {  # TODO
+        "name": "name of our license",
+        "url": "http://www.our-license-tbd.com"
+    }
+    openapi_schema['info']['contact'] = {  # TODO
+        "name": "alex wagner",
+        "email": "his email"
+    }
     app.openapi_schema = openapi_schema
     return app.openapi_schema
 
 
 app.openapi = custom_openapi
 
+# endpoint description text
+read_query_summary = """Given query, provide matches and match ratings from
+                     aggregated sources"""
+response_description = "A response to a validly-formed query"
+q_descr = "Gene to normalize."
+keyed_descr = """Optional. If true, return response as key-value pairs of
+              sources to source matches. False by default"""
+incl_descr = """Optional. Comma-separated list of source names to include in
+             response. Will exclude all other sources. Returns HTTP status code
+             422: Unprocessable Entity if both 'incl' and 'excl' parameters
+             are given."""
+excl_descr = """Optional. Comma-separated list of source names to exclude in
+             response. Will include all other sources. Returns HTTP status
+             code 422: Unprocessable Entity if both 'incl' and 'excl'
+             parameters are given."""
+
 
 @app.get("/search",
-         summary="Given query, provide matches and match ratings from "
-                 "aggregated sources",
+         summary=read_query_summary,
          operation_id="getQueryResponse",
-         response_description="A response to a validly-formed query",  # TODO
+         response_description=response_description,
          )
-def read_query(q: str,  # noqa: D103
-               keyed: Optional[bool] = False,
-               incl: Optional[str] = '',
-               excl: Optional[str] = ''):
+def read_query(q: str = Query(..., description=q_descr),  # noqa: D103
+               keyed: Optional[bool] = Query(False, description=keyed_descr),
+               incl: Optional[str] = Query('', description=incl_descr),
+               excl: Optional[str] = Query('', description=excl_descr)):
+    """Normalize gene string given by user.
+
+    :param q: gene search term
+    :param keyed: if true, response is structured as key/value pair of
+        sources to source match lists.
+    :param incl: comma-separated list of sources to include, with all others
+        excluded. Raises HTTPException if both `incl` and `excl` are given.
+    :param excl: comma-separated list of sources exclude, with all others
+        included. Raises HTTPException if both `incl` and `excl` are given.
+
+    :returns: JSON response with matched records and source metadata
+    """
     try:
-        resp = normalize(html.unescape(q), keyed=keyed, incl=incl, excl=excl)
+        resp = Normalizer.normalize(html.unescape(q), keyed=keyed, incl=incl,
+                                    excl=excl)
     except InvalidParameterException as e:
         raise HTTPException(status_code=422, detail=str(e))
 
