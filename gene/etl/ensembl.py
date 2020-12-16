a6650fd17887a81a7d0733dad21a988ff43f8d78
Kori Kuzma <42851808+korikuzma@users.noreply.github.com>
2020-12-16 16:09:34 -0500

    Update gene/etl/ensembl.py
    Co-authored-by: james stevenson <james.stevenson@nationwidechildrens.org>

--- gene/etl/ensembl.py
+++ gene/etl/ensembl.py
@@ -68,8 +68,8 @@ class Ensembl(Base):
         if 'data_path' in kwargs:
             self._data_src = kwargs['data_path']
         else:
-            wd_dir = PROJECT_ROOT / 'data' / 'ensembl'
-            wd_dir.mkdir(exist_ok=True, parents=True)  # TODO needed?
+            ensembl_dir = PROJECT_ROOT / 'data' / 'ensembl'
+            ensembl_dir.mkdir(exist_ok=True, parents=True)
             try:
                 self._data_src = sorted(list(wd_dir.iterdir()))[-1]
             except IndexError:
