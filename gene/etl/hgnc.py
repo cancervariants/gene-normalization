6b1f38a841526f94543a867f7bcaf92cd718fc67
korikuzma <korikuzma@gmail.com>
2020-12-22 13:00:11 -0500

    Move HGNC method

--- gene/etl/hgnc.py
+++ gene/etl/hgnc.py
@@ -111,62 +111,6 @@ class HGNC(Base):
                 assert Gene(**gene)
                 self._load_dynamodb(gene, batch)
 
-    def _get_location(self, r, gene):
-        """Store location in a gene record.
-
-        :param dict r: A gene record in the HGNC data file
-        :param dict gene: A transformed gene record
-        """
-        if 'location' in r:
-            contains_loc = True
-            location = {
-                'interval': {
-                    'type': IntervalType.CYTOBAND.value
-                },
-                'species_id': 'taxonomy:9606',
-                'type': LocationType.CHROMOSOME.value
-            }
-
-            # TODO: Check if this should be included
-            #       What to do if includes both location and annotation
-            annotations = {v.value for v in Annotation.__members__.values()}
-            for annotation in annotations:
-                if annotation in r['location']:
-                    location['annotation'] = annotation
-
-                    # Check if location is also included
-                    r['location'] = r['location'].split(annotation)[0].strip()
-                    if not r['location']:
-                        contains_loc = False
-
-            if contains_loc:
-                arm_match = re.search('[pq]', r['location'])
-
-                if arm_match:
-                    # Location gives arm and sub band
-                    arm_ix = arm_match.start()
-                    location['chr'] = r['location'][:arm_ix]
-
-                    if '-' in r['location']:
-                        # Location gives both start and end
-                        range_ix = re.search('-', r['location']).start()
-                        location['interval']['start'] = \
-                            r['location'][arm_ix:range_ix]
-                        location['interval']['end'] = \
-                            r['location'][range_ix + 1:]
-                    else:
-                        # Location only gives start
-                        start = r['location'][arm_ix:]
-                        location['interval']['start'] = start
-                else:
-                    # Location given is a single chromosome, i.e. 6
-                    location['chr'] = r['location']
-                    del location['interval']
-            else:
-                del location['interval']
-            gene['location'] = location
-            assert ChromosomeLocation(**gene['location'])
-
     def _load_dynamodb(self, gene, batch):
         """Insert gene records into DynamoDB gene_concepts table
 
@@ -312,6 +256,62 @@ class HGNC(Base):
             src_type.append(
                 f"{NamespacePrefix[key.upper()].value}"
                 f":{r[src]}")
+
+    def _get_location(self, r, gene):
+        """Store location in a gene record.
+
+        :param dict r: A gene record in the HGNC data file
+        :param dict gene: A transformed gene record
+        """
+        if 'location' in r:
+            contains_loc = True
+            location = {
+                'interval': {
+                    'type': IntervalType.CYTOBAND.value
+                },
+                'species_id': 'taxonomy:9606',
+                'type': LocationType.CHROMOSOME.value
+            }
+
+            # TODO: Check if this should be included
+            #       What to do if includes both location and annotation
+            annotations = {v.value for v in Annotation.__members__.values()}
+            for annotation in annotations:
+                if annotation in r['location']:
+                    location['annotation'] = annotation
+
+                    # Check if location is also included
+                    r['location'] = r['location'].split(annotation)[0].strip()
+                    if not r['location']:
+                        contains_loc = False
+
+            if contains_loc:
+                arm_match = re.search('[pq]', r['location'])
+
+                if arm_match:
+                    # Location gives arm and sub band
+                    arm_ix = arm_match.start()
+                    location['chr'] = r['location'][:arm_ix]
+
+                    if '-' in r['location']:
+                        # Location gives both start and end
+                        range_ix = re.search('-', r['location']).start()
+                        location['interval']['start'] = \
+                            r['location'][arm_ix:range_ix]
+                        location['interval']['end'] = \
+                            r['location'][range_ix + 1:]
+                    else:
+                        # Location only gives start
+                        start = r['location'][arm_ix:]
+                        location['interval']['start'] = start
+                else:
+                    # Location given is a single chromosome, i.e. 6
+                    location['chr'] = r['location']
+                    del location['interval']
+            else:
+                del location['interval']
+            gene['location'] = location
+            assert ChromosomeLocation(**gene['location'])
 
     def _load_data(self, *args, **kwargs):
         """Load the HGNC source into normalized database."""
