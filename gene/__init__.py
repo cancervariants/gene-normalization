faccc8c7b7693e79e3ebacc699812c96311d685c
korikuzma <korikuzma@gmail.com>
2021-05-19 13:05:54 -0400

    Create constants

--- gene/__init__.py
+++ gene/__init__.py
@@ -19,3 +19,24 @@ class DownloadException(Exception):
     def __init__(self, *args, **kwargs):
         """Initialize exception."""
         super().__init__(*args, **kwargs)
+
+
+from gene.schemas import SourceName, NamespacePrefix, SourceIDAfterNamespace  # noqa: E402, E501
+# Sources we import directly (HGNC, Ensembl, NCBI)
+SOURCES = {source.value.lower(): source.value
+           for source in SourceName.__members__.values()}
+
+# Set of sources we import directly
+XREF_SOURCES = {src.lower() for src in SourceName.__members__}
+
+# use to fetch source name from schema based on concept id namespace
+# e.g. {'hgnc': 'HGNC'}
+PREFIX_LOOKUP = {v.value: SourceName[k].value
+                 for k, v in NamespacePrefix.__members__.items()
+                 if k in SourceName.__members__.keys()}
+
+# use to generate namespace prefix from source ID value
+# e.g. {'ensg': 'ensembl'}
+NAMESPACE_LOOKUP = {v.value.lower(): NamespacePrefix[k].value
+                    for k, v in SourceIDAfterNamespace.__members__.items()
+                    if v.value != ''}
