c4e5fa9ddf84b04f101cb8d198a5aab14e622715
korikuzma <korikuzma@gmail.com>
2021-05-20 09:45:05 -0400

    add name of logger to logs

--- gene/__init__.py
+++ gene/__init__.py
@@ -5,7 +5,7 @@ import logging
 PROJECT_ROOT = Path(__file__).resolve().parents[0]
 logging.basicConfig(
     filename='gene.log',
-    format='[%(asctime)s] %(levelname)s : %(message)s')
+    format='[%(asctime)s] - %(name)s - %(levelname)s : %(message)s')
 logger = logging.getLogger('gene')
 logger.setLevel(logging.DEBUG)
 
