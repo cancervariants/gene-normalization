2c465b4f4acf9bcffa087b8b9147dc62c6128937
korikuzma <korikuzma@gmail.com>
2021-07-26 16:24:02 -0400

    WIP: add tests for normalize endpoint

--- tests/unit/test_query.py
+++ tests/unit/test_query.py
@@ -14,11 +14,80 @@ def query_handler():
             self.query_handler = QueryHandler()
 
         def search(self, query_str, keyed=False, incl='', excl=''):
-            resp = self.query_handler.search_sources(query_str=query_str,
-                                                     keyed=keyed,
-                                                     incl=incl, excl=excl)
-            return resp
+            return self.query_handler.search(query_str=query_str, keyed=keyed,
+                                             incl=incl, excl=excl)
+
+        def normalize(self, query_str):
+            return self.query_handler.normalize(query_str)
+
     return QueryGetter()
+
+
+@pytest.fixture(scope='module')
+def normalized_braf():
+    """Return normalized Gene Descriptor for BRAF."""
+    return {
+        "id": "normalize.gene:BRAF",
+        "type": "GeneDescriptor",
+        "value": {
+            "id": "hgnc:1097",
+            "type": "Gene"
+        },
+        "label": "BRAF",
+        "xrefs": {
+            "ensembl:ENSG00000157764",
+            "ncbigene:673"
+        },
+        "alternate_labels": [
+            "B-Raf proto-oncogene, serine/threonine kinase",
+            "BRAF1",
+            "RAFB1",
+            "NS7",
+            "B-RAF1",
+            "B-raf"
+        ],
+        "extensions": [
+            {
+                "name": "symbol_status",
+                "value": "approved",
+                "type": "Extension"
+            },
+            {
+                "name": "associated_with",
+                "value": [
+                    "vega:OTTHUMG00000157457",
+                    "ucsc:uc003vwc.5",
+                    "ccds:CCDS5863",
+                    "ccds:CCDS87555",
+                    "uniprot:P15056",
+                    "pubmed:2284096",
+                    "pubmed:1565476",
+                    "cosmic:BRAF",
+                    "omim:164757",
+                    "orphanet:119066",
+                    "iuphar:1943",
+                    "ena.embl:M95712",
+                    "refseq:NM_004333"
+                ],
+                "type": "Extension"
+            },
+            {
+                "name": "chromosome_location",
+                "value": {
+                    "_id": "ga4gh:VCL.O6yCQ1cnThOrTfK9YUgMlTfM6HTqbrKw",
+                    "type": "ChromosomeLocation",
+                    "species_id": "taxonomy:9606",
+                    "chr": "7",
+                    "interval": {
+                        "end": "q34",
+                        "start": "q34",
+                        "type": "CytobandInterval"
+                    }
+                },
+                "type": "Extension"
+            }
+        ]
+    }
 
 
 @pytest.fixture(scope='module')
@@ -27,7 +96,32 @@ def num_sources():
     return len({s for s in SourceName})
 
 
-def test_query(query_handler, num_sources):
+def compare_gene_descriptor(test, actual):
+    """Test that actual and expected gene descriptors match."""
+    assert actual["id"] == test["id"]
+    assert actual["type"] == test["type"]
+    assert actual["value"] == test["value"]
+    assert actual["label"] == test["label"]
+    assert set(actual["xrefs"]) == set(test["xrefs"])
+    assert set(actual["alternate_labels"]) == set(test["alternate_labels"])
+    extensions_present = "extensions" in test.keys()
+    assert ("extensions" in actual.keys()) == extensions_present
+    if extensions_present:
+        assert len(actual["extensions"]) == len(test["extensions"])
+        for test_ext in test["extensions"]:
+            for actual_ext in actual["extensions"]:
+                if actual_ext["name"] == test_ext["name"]:
+                    assert isinstance(actual_ext["value"],
+                                      type(test_ext["value"]))
+                    if isinstance(test_ext["value"], list):
+                        assert set(actual_ext["value"]) == \
+                               set(test_ext["value"])
+                    else:
+                        assert actual_ext["value"] == test_ext["value"]
+                    assert actual_ext["type"] == test_ext["type"]
+
+
+def test_search_query(query_handler, num_sources):
     """Test that query returns properly-structured response."""
     resp = query_handler.search(' BRAF ')
     assert resp['query'] == 'BRAF'
@@ -36,7 +130,7 @@ def test_query(query_handler, num_sources):
     assert len(matches) == num_sources
 
 
-def test_query_keyed(query_handler, num_sources):
+def test_search_query_keyed(query_handler, num_sources):
     """Test that query returns properly-structured response."""
     resp = query_handler.search(' BRAF ', keyed=True)
     assert resp['query'] == 'BRAF'
@@ -45,7 +139,7 @@ def test_query_keyed(query_handler, num_sources):
     assert len(matches) == num_sources
 
 
-def test_query_inc_exc(query_handler, num_sources):
+def test_search_query_inc_exc(query_handler, num_sources):
     """Test that query incl and excl work correctly."""
     sources = "hgnc, ensembl, ncbi"
     resp = query_handler.search('BRAF', excl=sources)
@@ -67,7 +161,7 @@ def test_query_inc_exc(query_handler, num_sources):
     assert 'NCBI' in matches
 
 
-def test_invalid_parameter_exception(query_handler):
+def test_search_invalid_parameter_exception(query_handler):
     """Test that Invalid parameter exception works correctly."""
     with pytest.raises(InvalidParameterException):
         resp = query_handler.search('BRAF', keyed=True, incl='hgn')  # noqa: F841, E501
@@ -100,8 +194,9 @@ def test_ache_query(query_handler, num_sources):
     assert matches['NCBI']['match_type'] == MatchType.XREF
 
 
-def test_braf_query(query_handler, num_sources):
+def test_braf_query(query_handler, num_sources, normalized_braf):
     """Test that BRAF concept_id shows xref matches."""
+    # Search
     resp = query_handler.search('ncbigene:673', keyed=True)
     matches = resp['source_matches']
     assert len(matches) == num_sources
@@ -122,6 +217,10 @@ def test_braf_query(query_handler, num_sources):
     assert matches['HGNC']['match_type'] == MatchType.XREF
     assert matches['Ensembl']['match_type'] == MatchType.CONCEPT_ID
     assert matches['NCBI']['match_type'] == MatchType.XREF
+
+    # Normalize
+    resp = query_handler.normalize("BRAF")
+    compare_gene_descriptor(normalized_braf, resp["gene_descriptor"])
 
 
 def test_abl1_query(query_handler, num_sources):
