158fb24b7d871f1aa72348d5b65f1891cacc4f33
Zachary Flohr <zachary.flohr@case.edu>
2022-10-28 04:12:42 -0400

    Make necessary changes to get ETL of NCBI to work

--- gene/vrs_locations/sequence_location.py
+++ gene/vrs_locations/sequence_location.py
@@ -18,7 +18,12 @@ class SequenceLocation:
         :param str seqid: Sequence ID accession
         :return: List of aliases for seqid
         """
-        return sr.translate_alias(seqid)
+        aliases = []
+        try:
+            aliases = sr.translate_alias(seqid)
+        except KeyError as e:
+            logger.warning(f"SeqRepo raised KeyError: {e}")
+        return aliases
 
     def add_location(self, seqid, gene, params, sr):
         """Get a gene's Sequence Location.
@@ -31,6 +36,9 @@ class SequenceLocation:
         """
         location = dict()
         aliases = self.get_aliases(sr, seqid)
+        if not aliases:
+            return location
+
         sequence_id = [a for a in aliases if a.startswith('ga4gh')][0]
 
         if gene.start != '.' and gene.end != '.' and sequence_id:
