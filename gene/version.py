351180648944073268655b38038440d9fc3b0318
James Stevenson <james.stevenson@nationwidechildrens.org>
2023-09-29 21:07:38 -0400

    build: configure setuptools in pyproject.toml

--- gene/version.py
+++
@@ -1,2 +0,0 @@
-"""Gene normalizer version"""
-__version__ = "0.3.0-dev1"
---
+++ src/gene/version.py
@@ -0,0 +1,4 @@
+"""Gene normalizer version"""
+from importlib.metadata import version
+
+__version__ = version("gene-normalizer")
